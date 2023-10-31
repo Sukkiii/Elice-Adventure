5a5fc01a56cb6b568cc9855ff837652c91fe6908
sukki <knk951213@naver.com>
2023-10-31 21:13:51 +0900

    1031codingtest

--- xodingtest/programmers/32makeArray2.js
+++ xodingtest/programmers/32makeArray2.js
@@ -8,3 +8,22 @@ function solution(l, r) {
   if (filter.length === 0) filter.push(-1);
   return filter;
 }
+
+function gen50() {
+    let i = 1;
+
+    while(true) {
+        yield Number(Number(i).toString(2)) * 5;
+        i++;
+    }
+}
+function solution(l, r) {
+    const n = gen50();
+    let a = 0;
+    const arr = [];
+
+    while(a < l) { a = n.next().value; }
+    while(a <= r) { arr.push(a); a = n.next().value; }
+
+    return arr.length ? arr : [-1];
+}
