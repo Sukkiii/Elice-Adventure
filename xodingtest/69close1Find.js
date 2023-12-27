588eb2f991a9e0a6055946c87691198d0568ac84
sukki <knk951213@naver.com>
2023-12-27 21:38:43 +0900

    1226codintest

---
+++ xodingtest/69close1Find.js
@@ -0,0 +1,10 @@
+function solution(arr, idx) {
+  for (let i = idx; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      return i
+    }
+  }
+  return -1
+}
+
+const solution = (a, i) => a.indexOf(1, i)
