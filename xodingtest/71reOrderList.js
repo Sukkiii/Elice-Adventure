a8f1c2f2af71227deac46116a661912298e70063
sukki <knk951213@naver.com>
2024-01-03 18:31:37 +0900

    0102 coding test

---
+++ xodingtest/71reOrderList.js
@@ -0,0 +1,8 @@
+function solution(num_list, n) {
+  return num_list.slice(n).concat(num_list.slice(0, n))
+}
+
+function solution(num_list, n) {
+  num_list.push(...num_list.splice(0, n))
+  return num_list
+}
