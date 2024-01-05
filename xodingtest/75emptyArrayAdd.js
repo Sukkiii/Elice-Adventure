6c6088ee596fe1a9e8ff44af32d4a36a259a4068
sukki <knk951213@naver.com>
2024-01-05 20:23:57 +0900

    0105 coding test

---
+++ xodingtest/75emptyArrayAdd.js
@@ -0,0 +1,13 @@
+const solution = (arr, flag) => {
+  const answer = []
+
+  for (let i = 0; i < flag.length; i++) {
+    if (flag[i]) {
+      answer.push(...Array(arr[i] * 2).fill(arr[i]))
+    } else {
+      answer.splice(-arr[i])
+    }
+  }
+
+  return answer
+}
