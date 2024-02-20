f7ba79fab2a62af7cd994a03f00031eaf5193d70
Sukki <knk951213@naver.com>
2024-02-20 19:18:16 +0900

    0220 level0 coding test

---
+++ xodingtest/programmers/Level0/numberCal.js
@@ -0,0 +1,11 @@
+// add 2 number
+const solution = (n1, n2) => n1 + n2
+
+// minus 2 number
+const solution2 = (n1, n2) => n1 - n2
+
+// multiple 2 number
+const solution3 = (n1, n2) => n1 * n2
+
+// % 2 number
+const solution4 = (n1, n2) => Math.floor(n1 / n2)
