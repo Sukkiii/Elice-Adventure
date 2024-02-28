4ab2e9ba46e7df53e3ba7b7bd72c0212ce08dcf1
sukki <knk951213@naver.com>
2024-02-28 15:36:13 +0900

    0228 level0 coding test

---
+++ xodingtest/programmers/Level0/calculatorByarrayLength.js
@@ -0,0 +1,15 @@
+function solution(arr, n) {
+  const result = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length % 2 === 0 && i % 2 !== 0) {
+      result.push(arr[i] + n)
+    } else if (arr.length % 2 !== 0 && i % 2 === 0) {
+      result.push(arr[i] + n)
+    } else {
+      result.push(arr[i])
+    }
+  }
+
+  return result
+}
